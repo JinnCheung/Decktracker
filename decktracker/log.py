467e3b5d30cd115be237fb8803b6664504565a2f
Jinn <rrzu@icloud.com>
2021-09-24 21:44:52 +0800

    0.03

--- decktracker/log.py
+++ decktracker/log.py
@@ -12,7 +12,7 @@ log = logging.getLogger(name="decktracker")
 log.setLevel(logging.getLevelName('DEBUG'))
 coloredlogs.install(level='DEBUG', fmt=log_format)
 
-# 2. 配置文件输出
+# 2. 配置文件输出???
 fileHandler = logging.FileHandler(os.path.join(os.getcwd(), 'debug.log'))
 fileHandler.setLevel(logging.getLevelName('INFO'))
 fileHandler.setFormatter(logging.Formatter(fmt=log_format, datefmt='%Y-%m-%d %H:%M:%S'))
