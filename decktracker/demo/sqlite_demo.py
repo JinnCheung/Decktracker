32ed0066c65d7a1c64cdb4e2f31d2fd7cce85314
Jinn <rrzu@icloud.com>
2021-09-28 11:01:51 +0800

    0.04

--- decktracker/demo/sqlite_demo.py
+++ decktracker/demo/sqlite_demo.py
@@ -11,13 +11,63 @@ more description
 """
 import os
 import sqlite3
-import subprocess
+from toml_demo import data_folder
+
+
+def create_table():
+    # 连接到SQLite数据库
+    # 如果文件不存在，会自动创建:
+    conn = sqlite3.connect(os.path.join(data_folder, 'test.db'))
+    # 创建一个Cursor:
+    cursor = conn.cursor()
+    # 执行一条SQL语句，创建user表:
+    cursor.execute('create table user (id varchar(20) primary key, name varchar(20))')
+    # 关闭Cursor:
+    cursor.close()
+    # 提交事务:
+    conn.commit()
+    # 关闭Connection:
+    conn.close()
+
+
+def insert_row():
+    # 连接到SQLite数据库
+    # 如果文件不存在，会自动创建:
+    conn = sqlite3.connect(os.path.join(data_folder, 'test.db'))
+    # 创建一个Cursor:
+    cursor = conn.cursor()
+    # 继续执行一条SQL语句，插入一条记录:
+    cursor.execute('insert into user (id, name) values (\'1\', \'Michael\')')
+    # 关闭Cursor:
+    cursor.close()
+    # 提交事务:
+    conn.commit()
+    # 关闭Connection:
+    conn.close()
+
+
+def query():
+    # 连接到SQLite数据库
+    # 如果文件不存在，会自动创建:
+    conn = sqlite3.connect(os.path.join(data_folder, 'test.db'))
+    with conn:
+        cur = conn.cursor()
+        cur.execute('SELECT SQLITE_VERSION()')
+
+        data = cur.fetchone()[0]
+
+        print("SQLite version: %s" % data)
+
+        cur.execute('SELECT * FROM user')
+
+        for row in cur.fetchall():
+            print("%s %s" % (row[0], row[1]))
 
 
 if __name__ == '__main__':
     # print(sqlite3.version)
     # print(sqlite3.sqlite_version)
-    print(os.path.abspath(os.path.dirname(os.getcwd())))
-    print(os.path.abspath(os.path.join(os.path.dirname(__file__))))
-    print(subprocess.call(["ls", "-l"], shell=False))
-    # print(subprocess.call(["sqlite3", "test.db"], shell=False))
+    # print(os.path.abspath(os.path.dirname(os.getcwd())))
+    # print(os.path.abspath(os.path.join(os.path.dirname(__file__))))
+    # insert_row()
+    query()
--- decktracker/demo/toml_demo.py
+++ decktracker/demo/toml_demo.py
@@ -10,24 +10,27 @@
 import os
 import toml
 
+custom_config_file = "/Users/ourob/Public/Decktracker/config.toml"
+default_config_file = os.path.abspath(os.path.join(os.path.dirname(__file__), 'config.toml'))
+if os.path.exists(custom_config_file):
+    config_file = custom_config_file
+elif os.path.exists(default_config_file):
+    config_file = default_config_file
+else:
+    raise FileNotFoundError("config.toml not found. there is a sample of config.toml in /docs.")
+
+config = toml.load(config_file)
+
+if os.path.exists(config.get('folders').get('root_folder')):
+    data_folder = os.path.abspath(os.path.join(config.get('folders').get('root_folder'),
+                                               config.get('folders').get('data_folder')))
+else:
+    data_folder = os.path.abspath(os.path.join(os.path.dirname(__file__),
+                                               config.get('folders').get('data_folder')))
+
+if not os.path.exists(data_folder):
+    os.makedirs(data_folder)
+
 if __name__ == "__main__":
-    custom_config_file = "/Users/ourob/Public/Decktracker/config.toml"
-    default_config_file = os.path.abspath(os.path.join(os.path.dirname(__file__), 'config.toml'))
-    if os.path.exists(custom_config_file):
-        config_file = custom_config_file
-    elif os.path.exists(default_config_file):
-        config_file = default_config_file
-    else:
-        raise FileNotFoundError("config.toml not found. there is a sample of config.toml in /docs.")
+    print(data_folder)
 
-    config = toml.load(config_file)
-
-    if os.path.exists(config.get('folders').get('root_folder')):
-        data_folder = os.path.abspath(os.path.join(config.get('folders').get('root_folder'),
-                                                   config.get('folders').get('data_folder')))
-    else:
-        data_folder = os.path.abspath(os.path.join(os.path.dirname(__file__),
-                                                   config.get('folders').get('data_folder')))
-
-    if not os.path.exists(data_folder):
-        os.makedirs(data_folder)
